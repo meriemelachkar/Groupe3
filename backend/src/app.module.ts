835926816342350520abeedff52dcdfe702d03b0
unknown <kaouthar.asrar@gmail.com>
2025-10-08 11:24:02 +0200

    update_backend2

---
+++ backend/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TestModule } from './test/test.module';
+
+@Module({
+  imports: [
+    // 1️⃣ Charger les variables d'environnement globalement
+    ConfigModule.forRoot({
+      isGlobal: true, // rend disponible partout sans ré-importer
+      envFilePath: '.env', // chemin vers ton fichier .env
+    }),
+
+    // 2️⃣ Connexion à MongoDB avec l’URL du .env
+    MongooseModule.forRoot(process.env.MONGO_URI),
+
+    TestModule,
+
+    // 3️⃣ Modules de ton app (auth, users, etc.)
+  ],
+})
+export class AppModule {}
